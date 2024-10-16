4ce6a9de39bea2d153669ee76136d9bdf9124dc4
Gabriel Torres <gabrielst328@gmail.com>
2024-10-16 00:32:57 -0300

    feat: Adicionado componente de Projetos

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgesComponent } from '../../components/knowledges/knowledges.component';
 import { ExperiencesComponent } from '../../components/experiences/experiences.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnowledgesComponent, ExperiencesComponent],
+  imports: [HeaderComponent, KnowledgesComponent, ExperiencesComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
