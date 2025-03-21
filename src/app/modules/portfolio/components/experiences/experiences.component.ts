52135ee57f2612c4a0e9a999807677a5b47193ad
Gabriel Torres <gabrielst328@gmail.com>
2025-03-21 15:05:21 -0300

    feat: experiences component style

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -15,9 +15,9 @@ export class ExperiencesComponent {
     {
       summary: {
         strong: 'Estágio Desenvolvedor Full-Stack',
-        p: 'Arcnet | Out 2024 - Até o momento',
+        p: 'Arcnet Desenvolvimento de Software Ltda. | Out 2024 - Até o momento',
       },
-      text: '<p>Atuei no desenvolvimento e na manutenção do aplicativo financeiro Nati Pay. Utilizei as tecnologias Node.js, Angular, Ionic e SQL Server</p>'
+      text: '<p>Atuei ativamente no desenvolvimento da solução financeira Mobyo Pay, um sistema de pagamentos e relatórios financeiros que permite ao cliente realizar e controlar transações por meio de PIX e cartão de débito e crédito. Utilizei as tecnologias Node.js, NestJS, Angular, Ionic e Firestore.</p><p>Em seguida, trabalhei no sistema Mobyo Integrador, um sistema responsável por integrar pedidos de diferentes plataformas (como Ifood, Zé Delivery, etc.) à plataforma de controle de pedidos do Mobyo, garantindo que o cliente possa ter controle desses pedidos de um lugar só. Utilizei as mesmas tecnologias do Mobyo Pay, com adição do Electron para transformar a aplicação em um app desktop.</p><p>Por fim, desenvolvi o Mobyo Pay Api, um projeto que tive a honra de criar do zero aplicando todos os conhecimentos adquiridos e acrescentando um grande detalhe à estruturação: a Clean Architecture. A aplicação permite integrar diversas formas de pagamento (internas e externas) de maneira simples e escalável ao sistema Mobyo Pay.</p>',
     },
   ]);
 }
